10bb5a1bffe1d30e41690f06eadc45262fb6e6cf
BrayanMorenoBorjas <bmorenoborjas@gmail.com>
2025-04-04 20:44:08 -0600

    Add files via upload

---
+++ Practica_9/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Practica_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(60, 97);
+            label1.Name = "label1";
+            label1.Size = new Size(271, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Factorial de un número";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(360, 94);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(153, 40);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(192, 255, 192);
+            button1.Location = new Point(547, 91);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 44);
+            button1.TabIndex = 2;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 32;
+            listBox1.Location = new Point(363, 196);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(150, 228);
+            listBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(60, 469);
+            label2.Name = "label2";
+            label2.Size = new Size(243, 32);
+            label2.TabIndex = 4;
+            label2.Text = "Factorial del número";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(309, 466);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(82, 40);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(471, 466);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(89, 40);
+            textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(406, 469);
+            label3.Name = "label3";
+            label3.Size = new Size(48, 32);
+            label3.TabIndex = 6;
+            label3.Text = "es ";
+            label3.Click += label3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(708, 557);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Font = new Font("Comic Sans MS", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 5, 5, 5);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private ListBox listBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label3;
+    }
+}
